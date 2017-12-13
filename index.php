15c1ef60b5bd4f135b4a4a98f0c48a750e5faaea
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 11:08:50 +0100

    init commit from bash for PHP-Les-Fonctions-Exercice8

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les fonctions Exercice 8</title>
+  </head>
+  <body>
+<?php
+  function operation(){
+    $number1 = 10;
+    $number2 = 10;
+    $number3 = 10;
+    $number = $number1 + $number2 + $number3;
+    return $number;
+  }
+  echo operation();
+ ?>
+  </body>
+</html>
